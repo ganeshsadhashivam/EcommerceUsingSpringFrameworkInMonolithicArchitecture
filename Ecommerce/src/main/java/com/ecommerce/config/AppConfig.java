9da7b59e0b4fc7d835380b5c15603fee3c90b1ef
ganesh sadhashivam <ganeshsadhashivam@outlook.com>
2026-05-14 09:23:14 +0530

    Implemented Spring JDBC Template setup

--- Ecommerce/src/main/java/com/ecommerce/config/AppConfig.java
+++ Ecommerce/src/main/java/com/ecommerce/config/AppConfig.java
@@ -1,8 +1,13 @@
 package com.ecommerce.config;
 
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /*
@@ -52,6 +57,36 @@ This is part of IOC (Inversion of Control).
 @ComponentScan(basePackages = "com.ecommerce")
 public class AppConfig {
 
+	@Bean
+	public DataSource dataSource()
+	{
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		
+		ds.setDriverClassName( "com.mysql.cj.jdbc.Driver");
+		
+		 ds.setUrl(
+	                "jdbc:mysql://localhost:3306/ecommercespringframeworkdb"
+	        );
+
+	        ds.setUsername("root");
+
+	        ds.setPassword("root");
+	        
+	        return ds;
+	}
+	
+	@Bean
+    public JdbcTemplate jdbcTemplate() {
+
+        JdbcTemplate jdbcTemplate =
+                new JdbcTemplate();
+
+        jdbcTemplate.setDataSource(
+                dataSource()
+        );
+
+        return jdbcTemplate;
+    }
 }
 
 
--- Ecommerce/src/main/java/com/ecommerce/dao/ProductDAO.java
+++ Ecommerce/src/main/java/com/ecommerce/dao/ProductDAO.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.config.DBConfig;
@@ -16,54 +18,87 @@ public class ProductDAO {
 
 	private List<Product> products = new ArrayList<>();
 	
-	public ProductDAO() {
-
-        products.add(new Product(1, "Laptop", 55000));
-        products.add(new Product(2, "Phone", 25000));
-        products.add(new Product(3, "Headphones", 3000));
-
-    }
 	
-	public List<Product> getAllProducts()
-	{
-		List<Product> products = new ArrayList<>();
-		
-		String query = "select * from products";
-		
-		try
-		{
-		Connection con = DBConfig.getConnection();
-		
-		PreparedStatement	ps = 	con.prepareStatement(query);
-		
-		ResultSet rs = ps.executeQuery();
-		
-		while(rs.next())
-		{
-			Product product =
-                    new Product();
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	/*
+	 * spring-jdbc template
+	 */
+	public List<Product> getAllProducts() {
 
-            product.setId(
-                    rs.getInt("id")
-            );
+	    String query =
+	            "SELECT * FROM products";
 
-            product.setName(
-                    rs.getString("name")
-            );
+	    return jdbcTemplate.query(
 
-            product.setPrice(
-                    rs.getDouble("price")
-            );
+	            query,
 
-            products.add(product);
-		}
-		}
-		catch(Exception e) {
+	            (rs, rowNum) -> {
 
-	        e.printStackTrace();
-	    }
-		return products;
+	                Product product =
+	                        new Product();
+
+	                product.setId(
+	                        rs.getInt("id")
+	                );
+
+	                product.setName(
+	                        rs.getString("name")
+	                );
+
+	                product.setPrice(
+	                        rs.getDouble("price")
+	                );
+
+	                return product;
+	            }
+	    );
 	}
+	
+	
+	
+	
+//	public List<Product> getAllProducts()
+//	{
+//		List<Product> products = new ArrayList<>();
+//		
+//		String query = "select * from products";
+//		
+//		try
+//		{
+//		Connection con = DBConfig.getConnection();
+//		
+//		PreparedStatement	ps = 	con.prepareStatement(query);
+//		
+//		ResultSet rs = ps.executeQuery();
+//		
+//		while(rs.next())
+//		{
+//			Product product =
+//                    new Product();
+//
+//            product.setId(
+//                    rs.getInt("id")
+//            );
+//
+//            product.setName(
+//                    rs.getString("name")
+//            );
+//
+//            product.setPrice(
+//                    rs.getDouble("price")
+//            );
+//
+//            products.add(product);
+//		}
+//		}
+//		catch(Exception e) {
+//
+//	        e.printStackTrace();
+//	    }
+//		return products;
+//	}
 	
 	
 //	public Product getProductById(int id)
@@ -78,70 +113,130 @@ public class ProductDAO {
 //		
 //		return null;
 //	}
-	public Product getProductById(int id)
-	{
-		Product product = null;
-		
-		String query = "select * from products where id=?";
-		try
-		{
-			Connection con = DBConfig.getConnection();
-			
-			PreparedStatement  ps  = con.prepareStatement(query);
-			
-			ps.setInt(1, id);
-			
-			ResultSet rs = ps.executeQuery();
-			
-			if(rs.next())
-			{
-				product = new Product();
-				
-				product.setId(rs.getInt("id"));
-				
-				product.setName(rs.getString("name"));
-				
-				product.setPrice(rs.getDouble("price"));
-			}
-		}
-		 catch(Exception e) {
+	
+//	public Product getProductById(int id)
+//	{
+//		Product product = null;
+//		
+//		String query = "select * from products where id=?";
+//		try
+//		{
+//			Connection con = DBConfig.getConnection();
+//			
+//			PreparedStatement  ps  = con.prepareStatement(query);
+//			
+//			ps.setInt(1, id);
+//			
+//			ResultSet rs = ps.executeQuery();
+//			
+//			if(rs.next())
+//			{
+//				product = new Product();
+//				
+//				product.setId(rs.getInt("id"));
+//				
+//				product.setName(rs.getString("name"));
+//				
+//				product.setPrice(rs.getDouble("price"));
+//			}
+//		}
+//		 catch(Exception e) {
+//
+//		        e.printStackTrace();
+//		    }
+//		
+//		return product;
+//	}
+	
+	/*
+	 * spring-jdbc template
+	 */
+	public Product getProductById(int id) {
 
-		        e.printStackTrace();
-		    }
-		
-		return product;
+	    String query =
+	            "SELECT * FROM products WHERE id = ?";
+
+	    return jdbcTemplate.queryForObject(
+
+	            query,
+
+	            (rs, rowNum) -> {
+
+	                Product product =
+	                        new Product();
+
+	                product.setId(
+	                        rs.getInt("id")
+	                );
+
+	                product.setName(
+	                        rs.getString("name")
+	                );
+
+	                product.setPrice(
+	                        rs.getDouble("price")
+	                );
+
+	                return product;
+	            },
+
+	            id
+	    );
+	}
+	
+	/*
+	 * spring-jdbc template
+	 */
+	
+	
+	public void addProduct(Product product) {
+
+	    String query =
+	            "INSERT INTO products " +
+	            "VALUES (?, ?, ?)";
+
+	    jdbcTemplate.update(
+
+	            query,
+
+	            product.getId(),
+
+	            product.getName(),
+
+	            product.getPrice()
+	    );
 	}
 	
 	
-	public void addProduct(Product product)
-	{
+//	public void addProduct(Product product)
+//	{
 //		products.add(product);
-		
-		String query = "insert into products values (?,?,?)";
-		
-		try
-		{
-			Connection con = DBConfig.getConnection();
-			
-			PreparedStatement  ps  = con.prepareStatement(query);
-			
-			ps.setInt(1,product.getId());
-			
-			ps.setString(2, product.getName());
-			
-			ps.setDouble(3, product.getPrice());
-			
-			ps.executeUpdate();
-			
-			System.out.println(
-	                "Product Inserted Successfully"
-	        );
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+//		
+//		String query = "insert into products values (?,?,?)";
+//		
+//		try
+//		{
+//			Connection con = DBConfig.getConnection();
+//			
+//			PreparedStatement  ps  = con.prepareStatement(query);
+//			
+//			ps.setInt(1,product.getId());
+//			
+//			ps.setString(2, product.getName());
+//			
+//			ps.setDouble(3, product.getPrice());
+//			
+//			ps.executeUpdate();
+//			
+//			System.out.println(
+//	                "Product Inserted Successfully"
+//	        );
+//		}
+//		catch(Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
 	
 	
 //	public boolean deleteProductById(int id)
@@ -159,32 +254,53 @@ public class ProductDAO {
 //		return false;
 //	}
 	
-	public boolean deleteProductById(int id)
-	{
-		String query = "delete from products where id=?";
-		
-		try
-		{
-			Connection con = DBConfig.getConnection();
-			
-			PreparedStatement  ps  = con.prepareStatement(query);
-			
-			ps.setInt(1, id);
-			
-			int rowsAffected = ps.executeUpdate();
-			
-			if(rowsAffected>0)
-			{
-				return true;
-			}
-		}
-		 catch(Exception e) {
+	
+	/*
+	 * spring -jdbc template
+	 */
+	
+	public boolean deleteProductById(int id) {
 
-		        e.printStackTrace();
-		    }
-		
-		return false;
+	    String query =
+	            "DELETE FROM products WHERE id = ?";
+
+	    int rowsAffected = jdbcTemplate.update(
+
+	            query,
+
+	            id
+	    );
+
+	    return rowsAffected > 0;
 	}
+	
+	
+//	public boolean deleteProductById(int id)
+//	{
+//		String query = "delete from products where id=?";
+//		
+//		try
+//		{
+//			Connection con = DBConfig.getConnection();
+//			
+//			PreparedStatement  ps  = con.prepareStatement(query);
+//			
+//			ps.setInt(1, id);
+//			
+//			int rowsAffected = ps.executeUpdate();
+//			
+//			if(rowsAffected>0)
+//			{
+//				return true;
+//			}
+//		}
+//		 catch(Exception e) {
+//
+//		        e.printStackTrace();
+//		    }
+//		
+//		return false;
+//	}
 	
 	
 //	public boolean updateProduct(int id,Product updateProduct)
@@ -202,36 +318,62 @@ public class ProductDAO {
 //		return false;
 //	}
 	
-	public boolean updateProduct(int id,Product updateProduct)
-	{
-		String query  =  "UPDATE products SET name = ?, price = ? WHERE id = ?";
-		
-		try
-		{
-	       Connection con = DBConfig.getConnection();
-			
-			PreparedStatement  ps  = con.prepareStatement(query);
-			
-			ps.setString(1, updateProduct.getName());
-			
-			ps.setDouble(2, updateProduct.getPrice());
-			
-			ps.setInt(3, id);
-			
-			 int rowsAffected =
-		                ps.executeUpdate();
+	
+	
+	public boolean updateProduct(
+	        int id,
+	        Product updatedProduct) {
 
-		        if(rowsAffected > 0) {
+	    String query =
+	            "UPDATE products " +
+	            "SET name = ?, price = ? " +
+	            "WHERE id = ?";
 
-		            return true;
-		        }
-		}
-		catch(Exception e) {
+	    int rowsAffected = jdbcTemplate.update(
 
-	        e.printStackTrace();
-	    }
-		return false;
+	            query,
+
+	            updatedProduct.getName(),
+
+	            updatedProduct.getPrice(),
+
+	            id
+	    );
+
+	    return rowsAffected > 0;
 	}
+	
+	
+//	public boolean updateProduct(int id,Product updateProduct)
+//	{
+//		String query  =  "UPDATE products SET name = ?, price = ? WHERE id = ?";
+//		
+//		try
+//		{
+//	       Connection con = DBConfig.getConnection();
+//			
+//			PreparedStatement  ps  = con.prepareStatement(query);
+//			
+//			ps.setString(1, updateProduct.getName());
+//			
+//			ps.setDouble(2, updateProduct.getPrice());
+//			
+//			ps.setInt(3, id);
+//			
+//			 int rowsAffected =
+//		                ps.executeUpdate();
+//
+//		        if(rowsAffected > 0) {
+//
+//		            return true;
+//		        }
+//		}
+//		catch(Exception e) {
+//
+//	        e.printStackTrace();
+//	    }
+//		return false;
+//	}
 	
 	
 	
